c552283febc324147384e7a4d45c4a3cb5806376
ANISH KUMAR MAHATO <anish.mahato@bajratechnologies.com>
2023-04-11 13:10:58 +0545

    feat: add custom email validation along with specified domains

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :confirmable
 
   enum gender: { male: 0, female: 1, other: 2 }
+
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }, if: :domain_check 
+
+  private
+
+  def domain_check
+    return if Rails.application.config.approved_domains.any? { |word| email.end_with?(word) }
+
+    errors.add(:email, 'is not from a valid domain')
+  end
 end
