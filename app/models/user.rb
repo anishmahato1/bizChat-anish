30d399b64343efe5c5c26c4ce6f26d3d8759eae0
ANISH KUMAR MAHATO <anish.mahato@bajratechnologies.com>
2023-04-14 02:49:32 +0545

    feat: add validations for name and gender

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,13 @@ class User < ApplicationRecord
 
   enum gender: { male: 0, female: 1, other: 2 }
 
-  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }, if: :domain_check 
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }, if: :domain_is_ok
+  validates :name, presence: true, format: { with: /\A[a-zA-Z ]+\z/, message: 'only allows letters and spaces' }
+  validates :gender, presence: true, inclusion: { in: User.genders.keys }
 
   private
 
-  def domain_check
+  def domain_is_ok
     return if Rails.application.config.approved_domains.any? { |word| email.end_with?(word) }
 
     errors.add(:email, 'is not from a valid domain')
