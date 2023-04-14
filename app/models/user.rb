c8b23c3c6d3d52326cd7b4596bb98361fec05767
ANISH KUMAR MAHATO <anish.mahato@bajratechnologies.com>
2023-04-15 00:29:44 +0545

    feat: add feature to remove extra spaces from name and email

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
 
+  before_validation :squish_name_and_email_spaces
+
   enum gender: { male: 0, female: 1, other: 2 }
 
   validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }, if: :domain_is_ok
@@ -14,5 +16,9 @@ class User < ApplicationRecord
     return if Rails.application.config.approved_domains.any? { |word| email.end_with?(word) }
 
     errors.add(:email, 'is not from a valid domain')
+  end
+
+  def squish_name_and_email_spaces
+    %i[name email].each { |attr| send("#{attr}=", send(attr).squish) if send(attr).present? }
   end
 end
