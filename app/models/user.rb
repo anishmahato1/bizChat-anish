1508377168f8bdbbd34e54b2b08a0eb39e0a47d8
ANISH KUMAR MAHATO <anish.mahato@bajratechnologies.com>
2023-04-17 17:21:52 +0545

    feat: create join table between chat and user

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ApplicationRecord
 
   enum gender: { male: 0, female: 1, other: 2 }
 
+  has_and_belongs_to_many :chats
+
   validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }, if: :domain_is_ok
   validates :name, presence: true, format: { with: /\A[a-zA-Z ]+\z/, message: 'only allows letters and spaces' }
   validates :gender, presence: true, inclusion: { in: User.genders.keys }
@@ -19,6 +21,6 @@ class User < ApplicationRecord
   end
 
   def squish_name_and_email_spaces
-    %i[name email].each { |attr| send("#{attr}=", send(attr).squish) if send(attr).present? }
+    [name, email].each { |attr| attr.squish! if attr.present? }
   end
 end
