4d0fc3c07caf64779fc1842d124028fd1206521f
ANISH KUMAR MAHATO <anish.mahato@bajratechnologies.com>
2023-04-18 01:44:13 +0545

    fix: configure route for after sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  resources :chats
+  devise_for :users, controllers: { registrations: "users/registrations" }
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root to: 'home#index'
+  root to: 'chats#index'
 end
