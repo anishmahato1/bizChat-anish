1cc068b341ad0c61ff4812d0c12b2b188586a1a3
ANISH KUMAR MAHATO <anish.mahato@bajratechnologies.com>
2023-04-21 10:22:54 +0545

    feat: add avatar for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,12 @@ class User < ApplicationRecord
   has_many :chats, through: :user_chats
 
   has_many :channels, through: :chats
+  has_one_attached :avatar do |attachable|
+    attachable.variant :thumb, resize_to_limit: [120, 150]
+    attachable.variant :small, resize_to_limit: [60, 60]
+  end
 
-
+  after_commit :add_default_avatar, on: %i[create update]
 
   validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }, if: :domain_is_ok
   validates :name, presence: true, format: { with: /\A[a-zA-Z ]+\z/, message: 'only allows letters and spaces' }
@@ -35,5 +39,16 @@ class User < ApplicationRecord
 
   def squish_name_and_email_spaces
     [name, email].each { |attr| attr.squish! if attr.present? }
+  end
+
+  def add_default_avatar
+    return if avatar.attached?
+
+    avatar.attach(
+      io: File.open(Rails.root.join('app', 'assets', 'images', 'default_avatar.jpg')),
+      filename: 'default_avatar.jpg',
+      content_type: 'image/jpg'
+    )
+    
   end
 end
