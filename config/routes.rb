bd76836d246007423fd5d79b9010ddccde3c6193
ANISH KUMAR MAHATO <anish.mahato@bajratechnologies.com>
2023-04-24 00:43:05 +0545

    feat: configure routes file for message and user resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: "users/registrations" }
-  resources :chats
-  get 'search', to: 'chats#search'
+  resources :chats do
+    resources :messages
+  end
+  get 'search', to: 'users#search'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
