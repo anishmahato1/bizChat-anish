5ef52c96aba2c5a8b6c79975ba0d1fe1f4537104
ANISH KUMAR MAHATO <anish.mahato@bajratechnologies.com>
2023-04-24 00:33:03 +0545

    feat: add image attachment for message

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,10 @@
 class Message < ApplicationRecord
+  default_scope { order(created_at: :asc) }
+
   belongs_to :sender, class_name: 'User'
   belongs_to :chat, touch: true
+
+  has_one_attached :image
+
+  scope :with_avatar_attached_of_sender, -> { includes(sender: { avatar_attachment: :blob }) }
 end
