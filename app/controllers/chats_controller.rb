780c31e2f043453654c72dd7b7bff10e0aa8883f
ANISH KUMAR MAHATO <anish.mahato@bajratechnologies.com>
2023-04-24 00:42:12 +0545

    feat: add show action for chats

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,44 +1,35 @@
 class ChatsController < ApplicationController
   layout 'homepage'
-  before_action :set_chat, only: %i[ show edit update destroy ]
-
+  before_action :set_chat, only: %i[show edit update destroy]
 
   def index
-    @channels = current_user.channels.with_chat_info_ordered
+    @channels = current_user.channels
     @q = User.ransack(params[:q])
-    @inboxes = current_user.chats.inboxes(current_user)
-  end
-
-  def search
-    @q = User.ransack(params[:q])
-    @users = @q.result(distinct: true)
-    respond_to do |format|
-      format.turbo_stream
-      format.html { render :search }
-    end
+    @inboxes = User.inboxes(current_user)
   end
 
 
-  # GET /chats/1 or /chats/1.json
   def show
+    @chat = Chat.find_by(id: params[:id])
+    @message = @chat.messages.new
+
+    @messages = Chat.messages_with_sender_avatar_included(@chat)
   end
 
-  # GET /chats/new
+
+
   def new
     @chat = Chat.new
   end
 
-  # GET /chats/1/edit
-  def edit
-  end
 
-  # POST /chats or /chats.json
+
   def create
     @chat = Chat.new(chat_params)
 
     respond_to do |format|
       if @chat.save
-        format.html { redirect_to chat_url(@chat), notice: "Chat was successfully created." }
+        format.html { redirect_to chat_url(@chat), notice: 'Chat was successfully created.' }
         format.json { render :show, status: :created, location: @chat }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -51,7 +42,7 @@ class ChatsController < ApplicationController
   def update
     respond_to do |format|
       if @chat.update(chat_params)
-        format.html { redirect_to chat_url(@chat), notice: "Chat was successfully updated." }
+        format.html { redirect_to chat_url(@chat), notice: 'Chat was successfully updated.' }
         format.json { render :show, status: :ok, location: @chat }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -65,19 +56,20 @@ class ChatsController < ApplicationController
     @chat.destroy
 
     respond_to do |format|
-      format.html { redirect_to chats_url, notice: "Chat was successfully destroyed." }
+      format.html { redirect_to chats_url, notice: 'Chat was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_chat
-      @chat = Chat.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def chat_params
-      params.require(:chat).permit(:is_channel, :name)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_chat
+    @chat = Chat.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def chat_params
+    params.require(:chat).permit(:is_channel, :name)
+  end
 end
