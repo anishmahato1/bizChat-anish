9295735ae60f5e762758f92ab957f7f1f02d19b8
ANISH KUMAR MAHATO <anish.mahato@bajratechnologies.com>
2023-04-26 20:32:23 +0545

    feat: added channel resource in route file

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
   resources :chats do
     resources :messages
   end
+  resources :channels
   get 'search', to: 'users#search'
-
+  resources :users, only: :show
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
