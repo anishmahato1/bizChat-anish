7c79a9e748359683c8810d1f0b16ee65819815db
ANISH KUMAR MAHATO <anish.mahato@bajratechnologies.com>
2023-04-26 20:23:31 +0545

    feat: add channel and message create actions

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -1,15 +1,32 @@
 class ChannelsController < ApplicationController
-  def show; end
+  def new
+    @channel = Channel.new
+  end
 
   def create
-    channel=
-end
+    @chat = Chat.new(is_channel: true)
+    @channel = @chat.build_channel(channels_params)
+
+    set_admin_and_include_in_users
+
+    if @channel.save
+      render :new, status: :unprocessable_entity
+    else
+      render :create, flash.now[:notice] = 'Channel Created Successfully!'
+    end
+  end
 
   def destroy; end
 
   private
-      # Only allow a list of trusted parameters through.
-      def channels_params
-        params.require(:channel).permit(:name, :description)
-      end
+
+  # Only allow a list of trusted parameters through.
+  def channels_params
+    params.require(:channel).permit(:name, :description)
+  end
+
+  def set_admin_and_include_in_users
+    @channel.admin = current_user
+    @chat.users << @channel.admin
+  end
 end
--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -8,58 +8,53 @@ class ChatsController < ApplicationController
     @inboxes = User.inboxes(current_user)
   end
 
-
   def show
-    @chat = Chat.find_by(id: params[:id])
-    @message = @chat.messages.new
-
-    @messages = Chat.messages_with_sender_avatar_included(@chat)
+    set_chat
+    set_recepient_info unless @chat.is_channel
+    @messages = Chat.messages_with_image_and_sender_avatar_included(@chat)
   end
-
-
 
   def new
     @chat = Chat.new
   end
 
-
-
   def create
     @chat = Chat.new(chat_params)
-
-    respond_to do |format|
-      if @chat.save
-        format.html { redirect_to chat_url(@chat), notice: 'Chat was successfully created.' }
-        format.json { render :show, status: :created, location: @chat }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @chat.errors, status: :unprocessable_entity }
-      end
-    end
   end
 
-  # PATCH/PUT /chats/1 or /chats/1.json
-  def update
-    respond_to do |format|
-      if @chat.update(chat_params)
-        format.html { redirect_to chat_url(@chat), notice: 'Chat was successfully updated.' }
-        format.json { render :show, status: :ok, location: @chat }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @chat.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  #   respond_to do |format|
+  #     if @chat.save
+  #       format.html { redirect_to chat_url(@chat), notice: 'Chat was successfully created.' }
+  #       format.json { render :show, status: :created, location: @chat }
+  #     else
+  #       format.html { render :new, status: :unprocessable_entity }
+  #       format.json { render json: @chat.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
-  # DELETE /chats/1 or /chats/1.json
-  def destroy
-    @chat.destroy
+  # # PATCH/PUT /chats/1 or /chats/1.json
+  # def update
+  #   respond_to do |format|
+  #     if @chat.update(chat_params)
+  #       format.html { redirect_to chat_url(@chat), notice: 'Chat was successfully updated.' }
+  #       format.json { render :show, status: :ok, location: @chat }
+  #     else
+  #       format.html { render :edit, status: :unprocessable_entity }
+  #       format.json { render json: @chat.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
-    respond_to do |format|
-      format.html { redirect_to chats_url, notice: 'Chat was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
+  # # DELETE /chats/1 or /chats/1.json
+  # def destroy
+  #   @chat.destroy
+
+  #   respond_to do |format|
+  #     format.html { redirect_to chats_url, notice: 'Chat was successfully destroyed.' }
+  #     format.json { head :no_content }
+  #   end
+  # end
 
   private
 
@@ -71,5 +66,10 @@ class ChatsController < ApplicationController
   # Only allow a list of trusted parameters through.
   def chat_params
     params.require(:chat).permit(:is_channel, :name)
+  end
+
+  # to show recepient info for private chat in right panel
+  def set_recepient_info
+    @recepient = @chat.users.except_current_user(current_user).with_attached_avatar.first
   end
 end
